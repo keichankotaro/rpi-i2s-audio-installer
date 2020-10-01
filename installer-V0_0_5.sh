c339d405e5362af003152b35854a09fb6552ae21
keichankotaro <44016629+keisukekisu@users.noreply.github.com>
2020-10-01 22:08:25 +0900

    Update installer-V0_0_5.sh

--- installer-V0_0_5.sh
+++ installer-V0_0_5.sh
@@ -3,3 +3,9 @@ COL_LIGHT_GREEN='\e[1;32m'
 COL_LIGHT_RED='\e[1;31m'
 TICK="[${COL_LIGHT_GREEN}✓${COL_NC}]"
 CROSS="[${COL_LIGHT_RED}✗${COL_NC}]"
+DONE="${COL_LIGHT_GREEN} done!${COL_NC}"
+I="[info]"
+
+cd ~
+set -e
+GIT_1=`git clone https://github.com/PaulCreaser/rpi-i2s-audio.git`
